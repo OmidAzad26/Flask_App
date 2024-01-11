db9dcf2da19135f639a1c937f8b37dbe4e53275c
OmidAzad26 <156089052+OmidAzad26@users.noreply.github.com>
2024-01-10 23:54:50 -0800

    Omid
    Upload final filess

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+import os
+import pathlib
+import requests
+from flask import url_for
+
+app = Flask("Google Login App")
+app.secret_key = "CodeSpecialist.com"
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "513832242491-quo4q5t8jg0s3h7gu2uor8gmbhej6ar1.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/protected_area")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/protected_area")
+@login_is_required
+def protected_area():
+    return render_template("profile.html", username=session['name'])
+
+@app.route("/content")
+def content():
+    return render_template("content.html")
+
+@app.route("/content2")
+def content2():
+    return render_template("content2.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
